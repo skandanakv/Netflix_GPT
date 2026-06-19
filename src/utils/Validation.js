277278886c4b90d00505f0492931575893e3ac16
Skandana KV <skandanakv@gmail.com>
2026-06-19 23:31:51 +0530

    updated from name validation for signin page

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,11 +1,20 @@
 
 //we will use regex here - google it , copy paste 
-export const checkValidData = (email, password) => {
+export const checkValidData = (name, email, password, isSignedIn) => {
   const validateEmail =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
 
   const validatePassword =
     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password);
+
+       // Run only during Sign Up
+    if (!isSignedIn) {
+        const validateName = /^[A-Za-z ]{3,30}$/.test(name);
+
+        if (!validateName) {
+            return "Name should contain only letters and be 3-30 characters long";
+        }
+    }
 
   if (!validateEmail && !validatePassword) {
     return "Invalid Email and Password";
@@ -18,6 +27,8 @@ export const checkValidData = (email, password) => {
   if (!validatePassword) {
     return "Password is invalid";
   }
+
+ 
 
   return null; // Everything is valid
 };
